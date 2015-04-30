08f4bf56ae86e6204a33d8de21b90f56c3208167
Billy Rukh <billyrukh@gmail.com>
2015-04-30 18:40:56 -0500

    Implemented stringifyJSON functionality & passed tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,32 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+var canStringify = function(value) { //checks for falsey values & functions
+    return value !== undefined && typeof value !== 'function';
+}
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+    var result;
+
+    if (canStringify(obj)) {
+        if (Array.isArray(obj)) {
+            result = [];
+            for (var i = 0; i < obj.length; i++) {
+                result.push(stringifyJSON(obj[i]));
+            }
+            return "[" + result.join(",") + "]";
+        } else if (obj instanceof Object) {
+            result = [];
+            for (var key in obj) {
+                if (canStringify(obj[key])) {
+                    result.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+                }
+            }
+            return "{" + result.join(",") + "}";
+        } else if (typeof obj === "string") {
+            return "\"" + obj + "\"";
+        } else {
+            return "" + obj;
+        }
+    }
 };
